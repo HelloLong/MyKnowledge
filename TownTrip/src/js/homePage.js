4953c9d07b35d594080bae3ffc53aca8fb63f1b0
lh_xu <lh_xu@Ctrip.com>
2017-01-15 17:45:17 +0800

    amend towntrip

---
+++ TownTrip/src/js/homePage.js
@@ -0,0 +1,11 @@
+$(function() {
+	$('.room_name').click(function(){
+		$(".room_name").removeClass("btn-selected");
+		$(this).addClass("btn-selected");
+		
+		var roomName = $(this).data("room");
+		if(roomName === "bigRoomByRiver"){
+			$("#slides").
+		}
+	})
+});
